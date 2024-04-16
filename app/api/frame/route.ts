0476de7f5b79cb0e3679825600745c3761885fcb
Cheetah0x <niallcheetham@gmail.com>
2024-04-16 12:19:29 +0100

    add text

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -49,6 +49,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
       image: {
         src: `${NEXT_PUBLIC_URL}/MGLImage.png`,
       },
+      input: {
+        text: 'What Project do you want to attest to?',
+      },
       postUrl: `${NEXT_PUBLIC_URL}`,
     }),
   );
