3afadd9e01463deba07b5cb2054c42d13c1db3f7
Cheetah0x <niallcheetham@gmail.com>
2024-04-16 11:07:53 +0100

    update url

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
+export const NEXT_PUBLIC_URL = 'https://tut-frames-attest.vercel.app';
 
 export const OP_EAS_ADDRESS = '0x4200000000000000000000000000000000000021';
